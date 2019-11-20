f906420ad66801736793cb43b33fa205d9a3f429
Juhi Rajgopal <juhirajgopal@wireless-10-105-248-94.umd.edu>
2019-11-20 13:16:02 -0500

    read radio button answers not working

--- app/src/main/java/com/group/changemyview/DashboardActivity.kt
+++ app/src/main/java/com/group/changemyview/DashboardActivity.kt
@@ -3,32 +3,38 @@ package com.group.changemyview
 import android.app.ListActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ListView
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.view.View
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import android.widget.*
+
 
 class DashboardActivity : ListActivity() {
 
+
+    internal lateinit var mAdapter: QuestionListAdapter
     private var listView: ListView? = null
+
+    private var mAnswerRadioGroup: RadioGroup? = null
+
     var questionList = arrayListOf<QuestionItem>()
-    private var submitButton: Button? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard_activity)
 
-        var db = FirebaseDatabase.getInstance().getReference("Questions")
+        var index = 1
+
+        val db = FirebaseDatabase.getInstance().getReference("Questions")
         db.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (snapshot in dataSnapshot.children) {
                     val questionText = snapshot.key
-                    val questionItem = QuestionItem(1, questionText!!, QuestionItem.Answer.NO_ANSWER)
+                    val questionItem = QuestionItem(index, questionText!!, QuestionItem.Answer.NO_ANSWER)
                     questionList.add(questionItem)
+                    index++
                 }
 
             }
@@ -38,29 +44,28 @@ class DashboardActivity : ListActivity() {
         })
 
         listView = findViewById<ListView>(android.R.id.list)
-        val adapter = QuestionListAdapter(this, questionList)
-        listView!!.adapter = adapter
-
-//        submitButton = findViewById(R.id.submitButton)
-//
-//        submitButton!!.setOnClickListener() {
-//            fun onClick(View v) {
-//                processAnswers()
-//            }
-//        }
+        mAdapter = QuestionListAdapter(this, questionList)
+        listView!!.adapter = mAdapter
     }
 
     override fun getListView(): ListView? {
         return listView!!
     }
 
-    fun processAnswers() {
+    fun processAnswers(v: View) {
 
-        for (q in questionList) {
-            Log.i(TAG, "Question is ${q.mQuestion}")
-//            val answerGroup = findViewById<RadioGroup>(R.id.answerGroup)
-//            val selectedButton = answerGroup.checkedRadioButtonId as RadioButton
+        for (i in 0 until mAdapter.count) {
+            val data = mAdapter.getItem(i) as QuestionItem
+            
+            mAnswerRadioGroup = findViewById<View>(R.id.answerGroup) as RadioGroup
 
+            when {
+                mAnswerRadioGroup!!.checkedRadioButtonId == R.id.answerYes -> data.mAnswer = QuestionItem.Answer.YES
+                mAnswerRadioGroup!!.checkedRadioButtonId == R.id.answerNo -> data.mAnswer = QuestionItem.Answer.NO
+                else -> data.mAnswer = QuestionItem.Answer.NO_ANSWER
+            }
+
+            Log.i(TAG, "${data.mQuestion} " + data.mAnswer.toString())
         }
     }
 
--- app/src/main/java/com/group/changemyview/QuestionListAdapter.kt
+++ app/src/main/java/com/group/changemyview/QuestionListAdapter.kt
@@ -1,13 +1,12 @@
 package com.group.changemyview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 import java.util.*
 
 class QuestionListAdapter(private val mContext: Context,
@@ -16,12 +15,19 @@ class QuestionListAdapter(private val mContext: Context,
     private val mItems = dataSource
     private var mLayoutInflater: LayoutInflater = LayoutInflater.from(mContext)
 
+//    fun add(item: ToDoItem) {
+//
+//        mItems.add(item)
+//        notifyDataSetChanged()
+//
+//    }
+
     // Returns the number of QuestionItems
     override fun getCount(): Int {
         return mItems.size
     }
 
-    // Retrieve the specific ToDoItem at pos
+    // Retrieve the specific QuestionItem at pos
     override fun getItem(pos: Int): Any {
         return mItems[pos]
     }
@@ -32,22 +38,53 @@ class QuestionListAdapter(private val mContext: Context,
         return pos.toLong()
     }
 
+
+    @SuppressLint("SetTextI18n")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
 
-        Log.i(TAG, "${javaClass.simpleName}::entered getView()")
+        val viewHolder: ViewHolder
+        val newView: View
 
-        val newView = mLayoutInflater.inflate(R.layout.question_item, parent, false)
+        if (null == convertView) {
 
-        // get views
-        val mQuestionNumView = newView.findViewById(R.id.question_number) as TextView
-        val mQuestionTextView = newView.findViewById(R.id.question_text) as TextView
+            // TODO - Inflate the View for this ToDoItem
+            newView = mLayoutInflater.inflate(R.layout.question_item, parent, false)
 
-        // set data
+            viewHolder = ViewHolder()
+            newView.tag = viewHolder
+            viewHolder.mQuestionNumber = newView.findViewById(R.id.question_number)
+            viewHolder.mQuestionText = newView.findViewById(R.id.question_text)
+            viewHolder.mAnswerView = newView.findViewById(R.id.answerGroup)
+
+        } else {
+            viewHolder = convertView.tag as ViewHolder
+            viewHolder.mAnswerView!!.setOnCheckedChangeListener(null)
+            newView = convertView
+
+        }
+
+        // TODO - Fill in specific Question Item data
+        // Remember that the data that goes in this View
+        // corresponds to the user interface elements defined
+        // in the layout file
+        val storeViewHolder = newView.tag as ViewHolder
+
+        // TODO - Display Title in TextView
         val item = getItem(position) as QuestionItem
-        mQuestionNumView.text = "Question ${item.mQuestionNum.toString()}"
-        mQuestionTextView.text = item.mQuestion
+        storeViewHolder.mQuestionNumber!!.text = "Question ${item.mQuestionNum.toString()}"
+
+        // TODO - Display Priority in a TextView
+        storeViewHolder.mQuestionText!!.text = item.mQuestion
 
         return newView
+    }
+
+    internal class ViewHolder {
+        var position: Int = 0
+        var mItemLayout: RelativeLayout? = null
+        var mQuestionNumber: TextView? = null
+        var mQuestionText: TextView? = null
+        var mAnswerView: RadioGroup? = null
     }
 
     companion object {
